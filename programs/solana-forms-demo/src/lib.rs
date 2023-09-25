7871810a86d5c6dd1de04a6ea46e4a2e7f7f5d6d
yangjiefeng <yangjiefeng@gmail.com>
2023-09-25 23:13:45 +0800

    update demo code

--- programs/solana-forms-demo/src/lib.rs
+++ programs/solana-forms-demo/src/lib.rs
@@ -13,20 +13,20 @@ declare_id!("9dTbbtqwNo2KcECHDPX16sYNQNQ5dduEwFnGPG29mmDH");
 pub mod solana_forms_demo {
     use super::*;
 
-    pub fn initialize(
+    pub fn create(
         ctx: Context<Create>,
-        fr_5pqi: u128,
-        fr_duif: Vec<String>,
-        fr_6i34: Vec<String>,
-        fr_8xjs: String,
+        arg_1: u128,
+        arg_2: Vec<String>,
+        arg_3: Vec<String>,
+        arg_4: String,
     ) -> Result<()> {
         *ctx.accounts.main_form = MainForm {
             signer_address: *ctx.accounts.authority.key,
             version: 0,
-            fr_5pqi,
-            fr_duif,
-            fr_6i34,
-            fr_8xjs,
+            fr_5pqi: arg_1,
+            fr_duif: arg_2,
+            fr_6i34: arg_3,
+            fr_8xjs: arg_4,
         };
         emit!(MainFormCreated {
             signer_address: *ctx.accounts.authority.key
